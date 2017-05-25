4d60c936787b7f25eb32e3a7c9dde18a534f8ffd
Unknown <bmorri12@stu.jjc.edu>
2017-05-25 11:27:03 +0000

    bug fix

--- build/local/docker/run.sh
+++ build/local/docker/run.sh
@@ -7,19 +7,19 @@ docker stop `echo $SERVICES`
 docker rm `echo $SERVICES`
 
 echo "starting registry..."
-docker run -d --name registry --link etcd --link mysql pandocloud/pando-cloud registry -etcd http://etcd:2379 -rpchost internal:20034 -aeskey ABCDEFGHIJKLMNOPABCDEFGHIJKLMNOP -dbhost mysql -dbname PandoCloud -dbport 3306 -dbuser root -loglevel debug
+docker run -d --name registry --link etcd --link mysql xfocus/smartaquaculture registry -etcd http://etcd:2379 -rpchost internal:20034 -aeskey ABCDEFGHIJKLMNOPABCDEFGHIJKLMNOP -dbhost mysql -dbname PandoCloud -dbport 3306 -dbuser root -loglevel debug
 
 echo "starting devicemanager..."
-docker run -d --name devicemanager --link etcd --link redis pandocloud/pando-cloud devicemanager -etcd http://etcd:2379 -rpchost internal:20033 -redishost redis:6379 -loglevel debug
+docker run -d --name devicemanager --link etcd --link redis xfocus/smartaquaculture devicemanager -etcd http://etcd:2379 -rpchost internal:20033 -redishost redis:6379 -loglevel debug
 
 echo "starting controller..."
-docker run -d --name controller --link etcd --link mongo pandocloud/pando-cloud controller -etcd http://etcd:2379 -mongohost mongo -rpchost internal:20032 -loglevel debug
+docker run -d --name controller --link etcd --link mongo xfocus/smartaquaculture controller -etcd http://etcd:2379 -mongohost mongo -rpchost internal:20032 -loglevel debug
 
 echo "starting mqttaccess..."
-docker run -d --name mqttaccess -p 1883:1883 --link etcd pandocloud/pando-cloud mqttaccess -etcd http://etcd:2379 -tcphost :1883 -usetls -keyfile /go/src/github.com/PandoCloud/pando-cloud/pkg/server/testdata/key.pem -cafile /go/src/github.com/PandoCloud/pando-cloud/pkg/server/testdata/cert.pem -rpchost internal:20030 -loglevel debug
+docker run -d --name mqttaccess -p 1883:1883 --link etcd xfocus/smartaquaculture mqttaccess -etcd http://etcd:2379 -tcphost :1883 -usetls -keyfile /go/src/github.com/xfocus/smartaquaculture/pkg/server/testdata/key.pem -cafile /go/src/github.com/xfocus/smartaquaculture/pkg/server/testdata/cert.pem -rpchost internal:20030 -loglevel debug
 
 echo "starting httpaccess..."
-docker run -d --name httpaccess -p 443:443 --link etcd --link redis pandocloud/pando-cloud httpaccess -etcd http://etcd:2379 -httphost :443 -redishost redis:6379 -usehttps -keyfile /go/src/github.com/PandoCloud/pando-cloud/pkg/server/testdata/key.pem -cafile /go/src/github.com/PandoCloud/pando-cloud/pkg/server/testdata/cert.pem -loglevel debug
+docker run -d --name httpaccess -p 443:443 --link etcd --link redis xfocus/smartaquaculture httpaccess -etcd http://etcd:2379 -httphost :443 -redishost redis:6379 -usehttps -keyfile /go/src/github.com/xfocus/smartaquaculture/pkg/server/testdata/key.pem -cafile /go/src/github.com/xfocus/smartaquaculture/pkg/server/testdata/cert.pem -loglevel debug
 
 echo "starting apiprovider..."
-docker run -d --name apiprovider -p 8888:8888 --link etcd pandocloud/pando-cloud apiprovider -etcd http://etcd:2379 -httphost :8888 -loglevel debug
+docker run -d --name apiprovider -p 8888:8888 --link etcd xfocus/smartaquaculture apiprovider -etcd http://etcd:2379 -httphost :8888 -loglevel debug
