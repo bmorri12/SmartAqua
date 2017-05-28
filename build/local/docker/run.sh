4c0b597f6809fa6cae8e43705320267573f0983b
Unknown <bmorri12@stu.jjc.edu>
2017-05-28 10:59:09 +0000

    run

--- build/local/docker/run.sh
+++ build/local/docker/run.sh
@@ -6,8 +6,10 @@ echo "stopping all services..."
 docker stop `echo $SERVICES`
 docker rm `echo $SERVICES`
 
+sudo docker run --link mysql mysql sh -c 'exec mysql -hmysql -uroot -e"CREATE DATABASE SeaWater"'
+
 echo "starting registry..."
-docker run -d --name registry --link etcd --link mysql xfocus/smartaquaculture registry -etcd http://etcd:2379 -rpchost internal:20034 -aeskey ABCDEFGHIJKLMNOPABCDEFGHIJKLMNOP -dbhost mysql -dbname PandoCloud -dbport 3306 -dbuser root -loglevel debug
+docker run -d --name registry --link etcd --link mysql xfocus/smartaquaculture registry -etcd http://etcd:2379 -rpchost internal:20034 -aeskey ABCDEFGHIJKLMNOPABCDEFGHIJKLMNOP -dbhost mysql -dbname SeaWater -dbport 3306 -dbuser root -loglevel debug
 
 echo "starting devicemanager..."
 docker run -d --name devicemanager --link etcd --link redis xfocus/smartaquaculture devicemanager -etcd http://etcd:2379 -rpchost internal:20033 -redishost redis:6379 -loglevel debug
@@ -23,3 +25,5 @@ docker run -d --name httpaccess -p 443:443 --link etcd --link redis xfocus/smart
 
 echo "starting apiprovider..."
 docker run -d --name apiprovider -p 8888:8888 --link etcd xfocus/smartaquaculture apiprovider -etcd http://etcd:2379 -httphost :8888 -loglevel debug
+
+sudo docker run -it --name pdcfg -v `echo $(pwd)`:/root --link etcd bmorri12/SmartAqua pdcfg -etcd http://etcd:2379
